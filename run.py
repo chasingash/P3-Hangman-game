54d35c3c194f3ac2a6be089818ff6c2c6230e392
chasingash <chasingaisling@gmail.com>
2022-04-13 23:20:12 +0000

    Add random module

--- run.py
+++ run.py
@@ -1,4 +1,35 @@
 import random
 from words import words
+import string
 
-print(words)
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # keep track of what the player has guessed
+
+    # Get player input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already guessed this character. Try again')
+
+    else:
+        print(("Not a valid character. Please try again"))
+
+user_input = input('Type something:')
+print(user_input)
